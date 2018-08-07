56a0f26e5e84fb7b1a902971787c03001fe57fc3
Hector <hector.franco.aceituno@gmail.com>
2018-08-07 17:56:32 +0200

    Others examples

---
+++ 04_Others/example_v-for/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <ul>
+      <li v-for="(ingredient, i) in ingredients" :key="ingredient">{{ ingredient }} ({{ i }})</li>
+    </ul>
+    <button @click="ingredients.push('spices')">Add New</button>
+    <hr>
+    <ul>
+      <li v-for="(person, key) in persons" :key="key">
+        <div v-for="(value, key, index) in person" :key="key">{{ key }}: {{ value }} ({{ index }})</div>
+      </li>
+    </ul>
+    <span v-for="n in 10" :key="n">{{ n }}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "app",
+  data() {
+    return {
+      ingredients: ['meat', 'fruit', 'cookies'],
+      persons: [
+        { name: 'Max', age: 27, color: 'red' },
+        { name: 'Anna', age: 'unknown', color: 'blue' }
+      ]
+    };
+  }
+};
+</script>
+
+<style>
+</style>
